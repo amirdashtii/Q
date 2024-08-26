040658829f58b11c45d86fb5dbde9c9021455f2d
Amir Dashti <ahdashti@gmail.com>
2024-08-26 16:18:20 +0330

    Initialize flight-ticket-service with Echo setup and basic folder structure

---
+++ flight-ticket-service/cmd/main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+)
+
+func main() {
+	err := godotenv.Load(".env")
+	if err != nil {
+		log.Fatalf("Error loading .env file")
+	}
+
+	e := echo.New()
+	e.Use(middleware.Logger())
+	e.Logger.Fatal(e.Start(":" + os.Getenv("PORT")))
+}
